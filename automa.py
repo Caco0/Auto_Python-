0d0cef9b3666ad91a4dc466c5f2ade398ee60e49
Caco0 <cacotpds@gmail.com>
2025-06-02 20:47:27 -0300

    First Commit

---
+++ automa.py
@@ -0,0 +1,5 @@
+# 1º passo entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# 2º passo fazer login
+# 3º Importar base de dados
+# 4º Cadastrar um produto
+# 5º Repetir para todos os produtos
