3f2114e6f35d533cb252f8f71afdb5618b82cbdc
Caco0 <cacotpds@gmail.com>
2025-06-04 00:02:15 -0300

    creating commands for automatic registrations

--- automa.py
+++ automa.py
@@ -28,7 +28,45 @@ time.sleep(3)
 
 produtos = pd.read_csv("../Bot/Python Power Up/produtos.csv")
 
-print(produtos)
-
+# print(produtos)
 # 4º Cadastrar um produto
+for linha in produtos.index:
+    pyautogui.click(x=-1263, y=244)
+
+    codigo = str(produtos.loc[linha, "codigo"])
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+
+    marca = str(produtos.loc[linha, "marca"])
+    pyautogui.write(marca)
+    pyautogui.press("tab")
+
+    tipo = produtos.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+    pyautogui.press("tab")
+
+    categoria = str(produtos.loc[linha, "categoria"])
+    pyautogui.write(categoria)
+    pyautogui.press("tab")
+
+    preco_unitario = str(produtos.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
+    pyautogui.press("tab")
+
+    custo = str(produtos.loc[linha, "custo"])
+    pyautogui.write(custo)
+    pyautogui.press("tab")
+
+    obs = str(produtos.loc[linha, "obs"])
+    if obs != "nan":
+        pyautogui.write(obs)
+    else:
+        pyautogui.write("")
+
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+    pyautogui.scroll(10000)
+
+
 # 5º Repetir para todos os produtos
--- mouse_position.py
+++ mouse_position.py
@@ -3,4 +3,6 @@ import time
 
 time.sleep(5)
 
+pyautogui.scroll(10000)
+
 print(pyautogui.position())
